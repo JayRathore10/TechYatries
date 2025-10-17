71107e2145e9eeb258f4a19a7ede69b78d45bd39
Jay <jay881532@gmail.com>
2025-10-17 14:21:54 +0530

    Add TechYatries Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import {FeaturesSection} from './components/FeaturesSection';
 import { Header } from './components/Header';
 import { Cards } from './components/Cards';
 import { CardsHeading } from './components/CardsHeading';
+import techYatriesFavicon from '../src/assets/images/logo.png';
 import './App.css';
 
 function App() {
 
   return (
     <>
+
+      <link rel="icon" type="image/svg+xml" href={techYatriesFavicon} />
+
       <Header />
       <HeroSection />
       <CardsHeading />
