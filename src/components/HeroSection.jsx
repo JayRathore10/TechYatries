841f5ffd40516519f5df3bb42060940502318be7
Jay <jay881532@gmail.com>
2025-10-17 13:25:46 +0530

    Make HeroSection background auto changing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,34 +1,95 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./HeroSection.css";
-import bgImage from '../assets/images/hero-bg.png';
+// import bgImage from '../assets/images/hero-bg.png';
 function HeroSection() {
+
+  const [images, setImages] = useState([]);
+  const [currIndex, setCurrIndex] = useState(0);
+
+  const locations = [
+    "Taj Mahal", "Goa Beach", "Kerala Backwaters",
+    "Himachal Mountains", "Rajasthan Forts",
+    "Varanasi Ghats", "Mysore Palace", "Andaman Islands",
+    "Darjeeling Tea Gardens", "Sundarbans Mangroves"
+  ];
+
+  // Jay's Api key
+  const apiKey = "iyAWLiH47sEeinuQWkA20QBM6XBum5UuxvXcyI3H-XA";
+
+  useEffect(() => {
+    const fetchImages = async () => {
+
+      try {
+
+        let allImages = [];
+        for (let loc of locations) {
+          const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+            params: {
+              query: loc,
+              per_page: 3,
+            },
+            headers: {
+              Authorization: `Client-ID ${apiKey}`
+            }
+          })
+          const url = response.data.results.map((img) => (
+            img.urls.regular
+          ))
+          allImages = allImages.concat(url);
+        }
+        setImages(allImages);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchImages();
+  }, []);
+
+  useEffect(() => {
+    if (images.length === 0) return;
+    const interval = setInterval(() => {
+      setCurrIndex((prev) => (
+        (prev + 1) % images.length
+      ))
+    }, 4000);
+
+    return () => { clearInterval(interval) }
+  }, [images])
+
+  const bgImage = images[currIndex] || "";
+
   return (
-    <section
-      className="hero-section"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      <div className="overlay"></div>
-      <div className="hero-content">
-        <h1>
-          Discover the World's{" "}
-          <span className="highlight">Hidden Treasures</span>
-        </h1>
-        <p
-          style={{fontWeight : "bold"}}
-        >
-          Upload photos, recognize monuments instantly, and unlock rich cultural
-          stories. Your personal guide to heritage sites worldwide.
-        </p>
-        <div className="hero-buttons">
-          <button className="btn upload-btn">
-            <i className="hgi hgi-stroke hgi-upload-04"></i>
-            &nbsp;Upload Moments</button>
-          <button className="btn explore-btn">
+      <section
+        className="hero-section"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          // transition: `background-image 1s ease-in-out`
+        }}
+      >
+        <div className="overlay"></div>
+        <div className="hero-content">
+          <h1>
+            Discover the World's{" "}
+            <span className="highlight">Hidden Treasures</span>
+          </h1>
+          <p
+            style={{ fontWeight: "bold" }}
+          >
+            Upload photos, recognize monuments instantly, and unlock rich cultural
+            stories. Your personal guide to heritage sites worldwide.
+          </p>
+          <div className="hero-buttons">
+            <button className="btn upload-btn">
+              <i className="hgi hgi-stroke hgi-upload-04"></i>
+              &nbsp;Upload Moments</button>
+            <button className="btn explore-btn">
               <i className="search-icon hgi hgi-stroke hgi-search-01"></i>
               &nbsp;Explore Monuments</button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
 
